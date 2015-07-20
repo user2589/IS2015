94c9969e74447995c72b5764f021e22ef7393f37
Marat <valiev.m@gmail.com>
2015-07-20 21:01:15 +0300

    added correlation

--- lr.py
+++ lr.py
@@ -7,11 +7,10 @@ from scipy import stats
 import numpy as np
 
 
-
 if __name__ =='__main__':
-    parser = argparse.ArgumentParser(description="Compute linear regression parameters for data in CSV file")
+    parser = argparse.ArgumentParser(
+        description="Compute linear regression parameters for data in CSV file")
     parser.add_argument('csv', help='CSV file with data, train data in first column')
-
 
     args = parser.parse_args()
 
@@ -22,9 +21,13 @@ if __name__ =='__main__':
 
     reader = csv.reader(fh)
     data = list(reader)
+    # actually, it should be matrices
     train = np.array([float(line[0]) for line in  data[1:]])
     features = np.array([map(float, line[1:]) for line in  data[1:]])
 
     # actually it returns coeffs, residuals, rank, singular_values
     coeff = np.linalg.lstsq(features, train)[0]
-    print coeff
+
+    prediction = np.dot(features, coeff)
+    print "Regression coefficients:", coeff
+    print "Correlation: ", np.correlate(prediction, train)
